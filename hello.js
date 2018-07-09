e4959bfc92ee45eb16112cccfecb782ec83fd4c8
Student <student@Admins-MacBook-Air.local>
2018-07-09 11:59:27 -0700

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-// Author: FirstName LastName
+// Author: Orisegun Origunwa
+
+console.log('Hello Code Next!');
+console.log("It's a beautiful day for programming, isn't it?");
+console.log("Hey! What's 1 + 1?");
+console.log("It's " + 1 + 1 + " of course!");
+console.log("No, it's " + (1 + 1) + ", silly goose!");
